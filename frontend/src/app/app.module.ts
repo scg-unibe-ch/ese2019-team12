d800a36e907992e691df42e19de6de8f4cf3c232
linohess <lino.hess@bluewin.ch>
2019-10-09 11:47:55 +0200

    Further yeets of the todo-list
    Co-Authored-By: Luca Bulletti <ProfJMoriarty@users.noreply.github.com>

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,14 +10,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 
-import { TodoListComponent } from './todo-list/todo-list.component';
-import { TodoItemComponent } from './todo-list/todo-item/todo-item.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent,
-    TodoItemComponent
   ],
   entryComponents: [],
   imports: [
