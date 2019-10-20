2e611bfd1c28b0e4ab5a6ebfe7cb319dffc69cb8
DominikFischli <dominikfischli@bluewin.ch>
2019-10-20 20:05:52 +0200

    Select User by either email or username

--- backend/app/helpers/session.helper.ts
+++ backend/app/helpers/session.helper.ts
@@ -1,17 +1,21 @@
 import { getSessionToken } from '../helpers/crypt.helper';
 import { User } from '../models/user.model';
 
+const regEmail = new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/');
+
 export async function getTokenOrNull(login: string, password: string){
   let token = null;
-  await User.findOne({
-    where: {
-      'email': login
-    }
-  }).then(instance => {
-    if(instance) {
+  let column = '';
+  column = regEmail.test(login) ? 'email' : 'username';
+  searchByColumn(column, login).then(instance => {
+    if(instance){
       const id = instance.toSimplification().id;
       token = getSessionToken(String(id));
     }
   });
   return token;
+}
+
+async function searchByColumn(column: string, login: string){
+  return await User.findOne({ where: { 'email': login }});
 }
