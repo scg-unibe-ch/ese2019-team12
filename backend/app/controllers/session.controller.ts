ea6e48de6bff914aea6321cdbfa260c04967f466
DominikFischli <dominikfischli@bluewin.ch>
2019-10-21 21:32:44 +0200

    fixed token get route

--- backend/app/controllers/session.controller.ts
+++ backend/app/controllers/session.controller.ts
@@ -3,17 +3,22 @@ import { getTokenOrNull } from '../helpers/session.helper';
 
 const router: Router = Router();
 
+const regSessionId = new RegExp('SESSIONID=');
+
+console.log(regSessionId.test("SESSIONID=21324"));
+
 router.get('/authenticate', async (req: Request, res: Response) => {
   const login = req.params.login;
   const password = req.params.password;
 
-  const token = getTokenOrNull(login, password);
+  const token = await getTokenOrNull(login, password);
 
   if(token){
-    res.cookie('SESSIONID', token, /*{ httpOnly: true, secure: true}*/);
+    res.cookie('SESSIONID', token, { httpOnly: true, /*secure: true*/});
   } else {
     res.sendStatus(401);
   }
+  res.status(200);
 });
 
 export const SessionController: Router = router;
