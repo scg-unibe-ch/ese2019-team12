cd1f8d96e62049679def3d8d2188e2ed47827235
DominikFischli <dominikfischli@bluewin.ch>
2019-10-21 21:35:32 +0200

    added method to extract token from cookie

--- backend/app/helpers/session.helper.ts
+++ backend/app/helpers/session.helper.ts
@@ -1,7 +1,11 @@
-import { getSessionToken } from '../helpers/crypt.helper';
+import { Request } from 'express';
+const expressJwt = require('express-jwt');
+
+import { getSessionToken, cert_pub } from '../helpers/crypt.helper';
 import { User } from '../models/user.model';
 
 const regEmail = new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/');
+const regSessionId = new RegExp('SESSIONID=');
 
 export async function getTokenOrNull(login: string, password: string){
   let token = null;
@@ -15,7 +19,20 @@ export async function getTokenOrNull(login: string, password: string){
   });
   return token;
 }
-
+export const checkIfAuthenticated = expressJwt({
+  secret: cert_pub,
+  getToken: extractToken
+});
+function extractToken(req: Request){
+  let cookies = req.headers.cookie;
+  if(cookies){
+    for(let cookie of cookies.split(';')) {
+      if(regSessionId.test(cookie)){
+        return cookie.split('=')[1];
+      }
+    }
+  }
+}
 async function searchByColumn(column: string, login: string){
   return await User.findOne({ where: { 'email': login }});
 }
