247a184c34d6320c86e6b38e6f12d8592838cc42
DominikFischli <dominikfischli@bluewin.ch>
2019-10-21 21:34:31 +0200

    renamed keys. made public key available for other modules

--- backend/app/helpers/crypt.helper.ts
+++ backend/app/helpers/crypt.helper.ts
@@ -1,8 +1,10 @@
 import * as fs from 'fs';
 import * as jwt from 'jsonwebtoken';
 import * as path from 'path';
+const expressJwt = require('express-jwt');
 
 const cert_priv = fs.readFileSync(path.join(process.cwd() + '/app/.conf', 'rsa-private-key.pem'));
+export const cert_pub = fs.readFileSync(path.join(process.cwd() + '/app/.conf', 'rsa-public-key.pem'));
 
 export function getSessionToken(userId: string) {
   return jwt.sign({}, cert_priv, {
