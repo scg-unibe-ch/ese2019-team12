98ea0d66bae2be14ffb2239ec39a0dfd4505b21d
Luca Bulletti <luca.bulletti@students.unibe.ch>
2019-10-23 13:14:06 +0200

    I undid the oopsie

--- frontend/src/app/_services/user.service.ts
+++ frontend/src/app/_services/user.service.ts
@@ -10,34 +10,38 @@ import { User } from '../_models/user';
 })
 export class UserService {
 
+  const apiUrl = 'http://localhost:3000';
+
   constructor(
     private httpClient: HttpClient
   ) { }
 
   getUsers(): Observable<User[]> {
-    return this.httpClient.get<User[]>('http://localhost:3000/users');
+    return this.httpClient.get<User[]>(apiUrl + '/users');
   }
   getUser(id: number): Observable<User> {
-    return this.httpClient.get<User>('http://localhost:3000/users/' + id);
+    return this.httpClient.get<User>(apiUrl + '/users/' + id);
   }
 
   create(user: User): Observable<User> {
-    return this.httpClient.post<User>('http://localhost:3000/users/', {
+    return this.httpClient.post<User>(apiUrl + '/users/', {
       firstname: user.firstname,
       lastname: user.lastname,
       email: user.email,
       password: user.password
     });
   }
+
   update(user: User): Observable<any> {
-    return this.httpClient.put('http://localhost:3000/users/' + user.id, {
+    return this.httpClient.put(apiUrl + '/users/' + user.id, {
       firstname: user.firstname,
       lastname: user.lastname,
       email: user.email,
       password: user.password
     });
   }
+
   delete(id: number): Observable<any> {
-    return this.httpClient.delete('http://localhost:3000/users' + id);
+    return this.httpClient.delete(apiUrl + '/users' + id);
   }
 }
