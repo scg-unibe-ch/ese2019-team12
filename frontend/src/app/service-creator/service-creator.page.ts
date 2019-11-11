1c5085c86094b35aab3ddc4e43cb259d9029e3f7
linohess <lino.hess@bluewin.ch>
2019-11-11 20:24:20 +0100

    Created sliders with buttons to change between sliders

--- frontend/src/app/service-creator/service-creator.page.ts
+++ frontend/src/app/service-creator/service-creator.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {IonSlides} from '@ionic/angular';
 
 
 
@@ -9,11 +10,18 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./service-creator.page.scss'],
 })
 export class ServiceCreatorPage implements OnInit {
+     @ViewChild('slides') slides: Slides;
 
 
   constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
+  nextSlide(){
+       this.slides.slideNext();
+  }
+  prevSlide() {
+         this.slides.slidePrev();
+     }
 
 }
