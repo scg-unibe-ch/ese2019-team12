e53df377db5f3881ed981905af49cde6320e82f9
Yael van Dok <yael.vandok@students.unibe.ch>
2019-11-13 14:28:28 +0100

    introduced event-cards

---
+++ frontend/src/app/_components/event-card/event-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-card',
+  templateUrl: './event-card.component.html',
+  styleUrls: ['./event-card.component.scss'],
+})
+export class EventCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
