fe513debe10d361c04a71580ac1842424b23a085
Yael van Dok <yael.vandok@students.unibe.ch>
2019-11-18 11:22:04 +0100

    routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MenuPage} from './menu/menu.page';
+import {MenuPageModule} from './menu/menu.module';
+import {ExplorePageModule} from './explore/explore.module';
+import {ExplorePage} from './explore/explore.page';
 
 const routes: Routes = [
   { path: '', loadChildren: './menu/menu.module#MenuPageModule' }
