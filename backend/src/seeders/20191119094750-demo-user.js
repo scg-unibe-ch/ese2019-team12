f2873e6bc7efeb2531479a324213db9dd642730c
DominikFischli <dominikfischli@bluewin.ch>
2019-11-20 12:46:44 +0100

    Demo Admin (password: hello)

--- backend/src/seeders/20191119094750-demo-user.js
+++ backend/src/seeders/20191119094750-demo-user.js
@@ -9,6 +9,16 @@ module.exports = {
       // password: hello
       password: 'd578ffbc858c13667c19aff5c8064c46eb7025ae6c6d240c457f9b0903ab165f',
       salt: 'xhFsLZbTBSTfnnL+8uxr8A==',
+    },
+    {
+      username: 'admin',
+      email: 'test@gmail.ch',
+      firstname: 'Dummy',
+      lastname: 'Hummy',
+      role: 'Admin',
+      // password: hello
+      password: 'd578ffbc858c13667c19aff5c8064c46eb7025ae6c6d240c457f9b0903ab165f',
+      salt: 'xhFsLZbTBSTfnnL+8uxr8A==',
     }], {});
 
   },
