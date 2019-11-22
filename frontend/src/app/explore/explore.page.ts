81d66d710f7deb256a52edcd006eb7867723e6ac
Luca Bulletti <luca.bulletti@students.unibe.ch>
2019-11-22 10:42:39 +0100

    added chips to explore page

--- frontend/src/app/explore/explore.page.ts
+++ frontend/src/app/explore/explore.page.ts
@@ -10,16 +10,14 @@ export class ExplorePage implements OnInit {
 
   // Placeholder for services that will be read from the database
   services = [
-      new Service(1, 'john_1', 'john\'s failing business', 'Catering', 1000, ''),
-      new Service(2, 'janette69', 'jane\'s trashy food', 'Catering', 200, 'sexy'),
-      new Service(3, 'JimmyJim', 'jim\'s clean up crew', 'Cleaning', 300, 'clean'),
-      new Service(4, 'john_1', 'john\'s security crew', 'Security', 5, 'safe AF')
+      new Service(1, 'john_1', 'john\'s failing business', 'Catering', 1000, ['yum', 'food']),
+      new Service(2, 'janette69', 'jane\'s trashy food', 'Catering', 200, ['sexy', 'trashy']),
+      new Service(3, 'JimmyJim', 'jim\'s clean up crew', 'Cleaning', 300, ['clean', 'murder']),
+      new Service(4, 'john_1', 'john\'s security crew', 'Security', 5, ['safe AF'])
   ];
 
+  constructor() {}
 
-  constructor() { console.log(this.services)}
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
