d7799a80d3dff103dcc90dbfb274850e4b3e0973
Luca Bulletti <luca.bulletti@students.unibe.ch>
2019-11-29 14:06:20 +0100

    how about saving the avatar in a png instead of using the base64 code

--- frontend/src/app/admin/admin.page.ts
+++ frontend/src/app/admin/admin.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SessionService } from '../_services/session.service';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPage implements OnInit {
 
-  constructor() { }
+    isLoggedIn: boolean;
+    currentUser: User;
 
-  ngOnInit() {
-  }
+    constructor(private sessionService: SessionService) {}
+
+    ngOnInit() {
+        this.isLoggedIn = this.sessionService.isLoggedIn();
+        if (this.isLoggedIn) {
+            this.currentUser = this.sessionService.getCurrentUser();
+        }
+    }
 
 }
