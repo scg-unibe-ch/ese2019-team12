5fddc93ab337fb175fbc7da8308a8a0a1eda9ea6
DominikFischli <dominikfischli@bluewin.ch>
2019-12-08 22:19:17 +0100

    set environment variables

--- backend/spec/support/run.js
+++ backend/spec/support/run.js
@@ -1,5 +1,9 @@
-import Jasmine from 'jasmine'
+import Jasmine from 'jasmine';
+import 'dotenv/config';
 
-var jasmine = new Jasmine()
-jasmine.loadConfigFile('spec/support/jasmine.json')
-jasmine.execute()
+process.env.DB_STORAGE='test.sqlite'; // Use the test db
+process.env.DB_LOG='None'; // Disable logging
+
+var jasmine = new Jasmine();
+jasmine.loadConfigFile('spec/support/jasmine.json');
+jasmine.execute();
